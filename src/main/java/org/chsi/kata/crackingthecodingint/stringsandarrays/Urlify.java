db30700a1168680400628f3238c5679c8f4c4a8b
Christopher Siciliano <christopher.siciliano@smals.be>
2018-02-01 15:48:47 +0100

    urlify kata

--- src/main/java/org/chsi/kata/crackingthecodingint/stringsandarrays/Urlify.java
+++ src/main/java/org/chsi/kata/crackingthecodingint/stringsandarrays/Urlify.java
@@ -1,10 +1,34 @@
 package org.chsi.kata.crackingthecodingint.stringsandarrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Urlify {
 
 
     public char[] encode(char[] content ) {
-        return content;
+
+        int newSize = content.length;
+
+        for(int i = 0; i < content.length; i++) {
+            if(content[i] == ' ')
+                newSize += 2;
+        }
+
+        if(newSize == content.length)
+            return content;
+
+        char[] updateContent = new char[newSize];
+
+        for(int i = 0, j = 0; i < content.length; i++, j++) {
+            if(content[i] == ' ') {
+                updateContent[j] = '%'; updateContent[++j] = '2'; updateContent[++j] = '0';
+            } else {
+                updateContent[j] = content[i];
+            }
+        }
+        return updateContent;
     }
 
 }
--- src/test/java/org/chsi/kata/crackingthecodingint/stringsandarrays/UrlifyTest.java
+++ src/test/java/org/chsi/kata/crackingthecodingint/stringsandarrays/UrlifyTest.java
@@ -2,12 +2,14 @@ package org.chsi.kata.crackingthecodingint.stringsandarrays;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class UrlifyTest {
 
     @Test
     public void encode() {
-        assertEquals("Hello the world", "Hello\\%20the\\%20world");
+        assertTrue(Arrays.equals(new char[] {'%','2','0','a','%','2','0','%','2','0','b','c','%','2','0'}, new Urlify().encode(new char[]{' ','a', ' ',' ', 'b', 'c', ' '})));
     }
 }
