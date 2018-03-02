bcd0e29af5fa6ee80ce8acfdf627a0af62b39042
chsi13 <siciliano02@gmail.com>
2018-03-02 16:43:30 +0100

    Update DigitalRoot.java

--- src/main/java/org/chsi/kata/codewars/DigitalRoot.java
+++ src/main/java/org/chsi/kata/codewars/DigitalRoot.java
@@ -1,7 +1,9 @@
 package org.chsi.kata.codewars;
 
 import java.util.Arrays;
-
+/**
+A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+**/
 public class DigitalRoot {
 
     public static int digital_root(int n) {
