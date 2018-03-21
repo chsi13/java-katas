00c47f8a151b0606e1c69272b4d385ada73ae29a
Christopher Siciliano <christopher.siciliano@smals.be>
2018-03-21 06:59:28 +0100

    nextSmaller

--- src/main/java/org/chsi/kata/codewars/NextSmallerNumberWithSameDigit.java
+++ src/main/java/org/chsi/kata/codewars/NextSmallerNumberWithSameDigit.java
@@ -1,27 +1,50 @@
 package org.chsi.kata.codewars;
 
+import java.util.*;
 
 public class NextSmallerNumberWithSameDigit {
+
     public static long nextSmaller(long n) {
-        if (n < 20)
+        if (n < 20) // fast precondition
             return -1;
         String numberAsString = String.valueOf(n);
-        int iteration = numberAsString.length();
-        while (iteration > 0) {
-            int startPointer = numberAsString.length() - 1;
-            while (startPointer > 0) {
-                char[] toChars = numberAsString.toCharArray();
-                char tmp = toChars[startPointer];
-                toChars[startPointer] = toChars[startPointer - 1];
-                toChars[startPointer - 1] = tmp;
-                long permutValue = Long.valueOf(String.valueOf(toChars));
-                if (permutValue < n && !String.valueOf(toChars).startsWith("0"))
-                    return permutValue;
-                startPointer--;
-                numberAsString = String.valueOf(toChars);
+        int length = numberAsString.length();
+        int pointer = length - 1;
+        while (pointer > 0) {
+            String substring = numberAsString.substring(pointer-- - 1, numberAsString.length());
+            String reorderString = reorder(substring, numberAsString.length());
+            if (!substring.equals(reorderString)) {
+                return Long.valueOf(numberAsString.substring(0, numberAsString.length() - substring.length()).concat(reorderString));
             }
-            iteration--;
         }
         return -1;
+    }
+
+    private static String reorder(String substring, int totalLength) {
+        char firstChar = substring.charAt(0);
+        char nextFirst = ' ';
+        int indexKeep = 0;
+        for (int i = 1; i < substring.length(); i++) {
+            if (substring.charAt(i) < firstChar) {
+                if (substring.length() == totalLength && substring.charAt(i) == '0') {
+                } else {
+                    if (nextFirst < substring.charAt(i)) {
+                        nextFirst = substring.charAt(i);
+                        indexKeep = i;
+                    }
+                }
+            }
+        }
+        if (nextFirst == ' ')
+            return substring;
+        else {
+            StringBuilder stringBuilder = new StringBuilder(substring);
+            stringBuilder.deleteCharAt(indexKeep); // remove previous first letter
+            char[] suffixAsArray = stringBuilder.toString().toCharArray();
+            Arrays.sort(suffixAsArray); // sort without nextFirst
+            String reverse = new StringBuilder(String.valueOf(suffixAsArray)).reverse().toString(); // reverse order
+            String result = String.valueOf(nextFirst + reverse);
+            return result;
+        }
     }
 }
--- src/test/java/org/chsi/kata/codewars/NextSmallerNumberWithSameDigitTest.java
+++ src/test/java/org/chsi/kata/codewars/NextSmallerNumberWithSameDigitTest.java
@@ -1,5 +1,6 @@
 package org.chsi.kata.codewars;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,6 +16,9 @@ public class NextSmallerNumberWithSameDigitTest {
         assertEquals(414, NextSmallerNumberWithSameDigit.nextSmaller(441));
         assertEquals(123456789, NextSmallerNumberWithSameDigit.nextSmaller(123456798));
         assertEquals(153, NextSmallerNumberWithSameDigit.nextSmaller(315));
-        assertEquals(117400, NextSmallerNumberWithSameDigit.nextSmaller(114700));
+        assertEquals(117040, NextSmallerNumberWithSameDigit.nextSmaller(117400));
+        assertEquals(1072, NextSmallerNumberWithSameDigit.nextSmaller(1207));
+        assertEquals(2558258224755L, NextSmallerNumberWithSameDigit.nextSmaller(2558258225457L));
+        assertEquals(1999999999999L, NextSmallerNumberWithSameDigit.nextSmaller(9199999999999L));
     }
 }
